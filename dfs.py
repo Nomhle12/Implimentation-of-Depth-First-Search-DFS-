bc8feb5315d0f1f10765c789ce03172b45c71470
Nomhle <122969691+Nomhle12@users.noreply.github.com>
2026-09-08 11:22:28 +0200

    Add DFS function for graph traversal
    Implement depth-first search algorithm to traverse a graph.

---
+++ dfs.py
@@ -0,0 +1,17 @@
+def dfs(graph,start):
+    visited = []
+
+    def visit(node):
+        visited.append(node)
+
+        for neighbor in range(len(graph)):
+            if graph[node][neighbor] == 1 and neighbor not in visited:
+                visit(neighbor)
+
+    visit(start)
+    return visited
+
+print(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]], 1))
+print(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]], 3))
+print(dfs([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0]], 3))
+print(dfs([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]], 0))
